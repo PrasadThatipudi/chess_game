cd18066cd1a48ebd3ad2a14c5353ae4cb08333a1
Abhinav K <183721743+AbhinavKaippa@users.noreply.github.com>
2025-03-18 10:22:28 +0530

    reverted changes

--- src/piece.js
+++ src/piece.js
@@ -3,27 +3,27 @@ class Piece {
   #color;
   #position;
 
-  constructor(type, color, position) {
+  constructor (type, color, position) {
     this.#type = type;
     this.#color = color;
     this.#position = position;
   }
 
-  get type() {
+  get type () {
     return this.#type;
   }
 
-  get color() {
+  get color () {
     return this.#color;
   }
 
-  set position(newPosition) {
-    this.#position = newPosition;
+  get position () {
+    return this.#position;
   }
 
-  get position() {
-    return this.#position;
+  set position (newPosition) {
+    this.#position = newPosition;
   }
 }
 
-export { Piece };
+export {Piece};
